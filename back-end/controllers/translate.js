fbd6c484221a3e468fd5de5160b8a5fdab859a76
jzhupan <jzhupan@gmail.com>
2023-04-24 12:14:56 -0400

    Found a better translator API, was able to translate the extracted text and display it on the front end. Aachieved final goal for this project.

--- back-end/controllers/translate.js
+++ back-end/controllers/translate.js
@@ -6,5 +6,49 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 const axios = require('axios');
 
+router.post('/', function (req, res) {
+    //this is the Template from rapidAPI google translate API
+    const encodedParams = new URLSearchParams();
+    encodedParams.set('q', req.body.recognizeText);
+    encodedParams.set('target', 'en');
+    encodedParams.set('source', req.body.languageSource);
+    
+    const options = {
+      method: 'POST',
+      url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept-Encoding': 'application/gzip',
+        'X-RapidAPI-Key': process.env.apiKey,
+        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+      },
+      data: encodedParams,
+    };
 
+    async function translateText() {
+        try {
+            const response = await axios.request(options);
+            console.log(response.data);
+            let translatedText = response.data.data.translations[0].translatedText;
+            console.log(response.data.data.translations[0].translatedText)
+
+            res.send({
+                'translatedText': translatedText
+            })
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
+    translateText()
+
+})
+
+router.get('/', (req, res) => {
+    res.send("You have hit the translate controller, yay!")
+});
+
+
+module.exports = router;
 
